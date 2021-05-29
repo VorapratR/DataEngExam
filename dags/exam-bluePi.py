fda0a99c10cebcc5fc2b1108f6334323f6974862
Voraprat Rattana <voraprat.r@dosetech.co>
2021-05-29 15:31:03 +0700

    [Connect] DB

--- dags/exam-bluePi.py
+++ dags/exam-bluePi.py
@@ -1,26 +1,62 @@
 import airflow
+import psycopg2
 
 from airflow.models import DAG
 from airflow.models import Variable
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+
+
+class Config:
+
+    POSTGRESQL_HOST = Variable.get("POSTGRESQL_HOST")
+    POSTGRESQL_PORT = Variable.get("POSTGRESQL_PORT")
+    POSTGRESQL_USER = Variable.get("POSTGRESQL_USER")
+    POSTGRESQL_PASSWORD = Variable.get("POSTGRESQL_PASSWORD")
+    POSTGRESQL_DB = Variable.get("POSTGRESQL_DB")
+
+
+def get_data_from_db():
+
+    # Connect to the database
+    connection = psycopg2.connect(user=Config.POSTGRESQL_USER,
+                                  password=Config.POSTGRESQL_PASSWORD,
+                                  host=Config.POSTGRESQL_HOST,
+                                  port=Config.POSTGRESQL_PORT,
+                                  database=Config.POSTGRESQL_DB)
+
+    # Create a cursor to perform database operations
+    cursor = connection.cursor()
+    # Print PostgreSQL details
+    print("PostgreSQL server information")
+    print(connection.get_dsn_parameters(), "\n")
+    return connection.get_dsn_parameters()
+
+
+# def display_variable():
+#     my_var = Variable.get("my_var")
+#     print('Variable: ' + my_var)
+#     return my_var
 
 default_args = {
-    'owner': 'datath',
-    'start_date': airflow.utils.dates.days_ago(1),
+    'owner': 'VorapratR',
+    'start_date': days_ago(1),
 }
 
+dag = DAG(
+    'exam_bluepi',
+    default_args=default_args,
+    description='Interview Challenge (Data Engineer) - 4 days',
+    schedule_interval=timedelta(days=1),
+)
 
-def display_variable():
-    my_var = Variable.get("my_var")
-    print('Variable: ' + my_var)
-    return my_var
+t1 = PythonOperator(
+    task_id='get_data_from_db',
+    python_callable=get_data_from_db,
+    dag=dag,
+)
 
-
-with DAG(dag_id='variable_id',
-         default_args=default_args,
-         schedule_interval="@once") as dag:
-
-    tasks = PythonOperator(
-        task_id='display_variable',
-        python_callable=display_variable
-    )
+t1
